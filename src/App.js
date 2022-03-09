52ab69bc43f09dfe8e37c2c08e111c39ec2b1912
Oger Carrillo <ogercarrillo@gmail.com>
2022-03-09 11:04:50 -0800

    done

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>
-          <PersonCard firstName={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Black"}/>
-          <PersonCard firstName={"John"} lastName={"Smith"} age={"88"} hairColor={"Brown"}/>
-          <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={"50"} hairColor={"Brown"}/>
-          <PersonCard firstName={"Maria"} lastName={"Smith"} age={"62"} hairColor={"Brown"}/>
+          <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+          <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+          <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+          <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
         </div>
       </header>
     </div>
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,25 @@
 import React, { Component } from  'react'; 
 
 class PersonCard extends Component{
+    // Declare constructor to overide default constructor so that we can initially set the state object
+    constructor(props) {
+        // gives us all the functionality  of the default constructor that comes with Component
+        super(props); 
+        this.state = {age: this.props.age}
+    }
+
+    increaseAge = () => {
+        this.setState({age: this.state.age + 1}); 
+    }
     render(){
         return (
             <div>
                 <h1>{this.props.firstName} {this.props.lastName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.increaseAge}>
+                    Birthday Button for {this.props.firstName} {this.props.lastName}
+                </button>
             </div>
         ); 
     }
